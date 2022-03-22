69b8af5f505cfedcfd5c006a7e2bd837730d3c69
Sagar Prajapati <psagc@allstate.com>
2022-03-22 07:58:42 +0530

    Initial Comment

---
+++ FNCodingTest/FNCodingTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FNCodingTest
+//
+//  Created by psagc on 22/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
